e00fa6d0781d012ad2ef0141bec498e8d9b20d1c
Daniel <danielshitaye10@gmail.com>
2025-08-07 15:53:30 +0300

    es6 arrow functions and their behaviour

--- D10_Classes_PrivateFields/example1.js
+++ D10_Classes_PrivateFields/example1.js
@@ -1,4 +1,10 @@
 class Person {
+
+    // the following 2 properties are on each instance 
+    // since we use  #they are being private
+    // that means we can not access them using . operator from the instance.
+    // so we have to write a class setter and getter methods to access them.
+
     #firstName;
     #lastName;
     
@@ -7,10 +13,17 @@ class Person {
         this.#lastName = lname;
     }
 
+    // the func fname help us to red the property firstName in the object 
+    // since we can not directly access the property from the instance 
+    // rather we call fname from the instance using the . operator to read its value.
+
     get fname() {
         return this.#firstName;
     }
 
+    // the func set fname help us to set a value for the property firstname of an instance 
+    // since we dont have direct access to this property using . operator.
+    
     set fname(fn) {
         this.#firstName = fn;
     }
@@ -21,7 +34,11 @@ class Person {
 }
 
 let me = new Person('Tony', 'Alicea');
+
 console.log(me.greet());
+
 me.fname = 'Anthony';
+
 console.log(me.greet());
+
 console.log(me.fname);
--- E3_ArrowFunctions_This/example1.js
+++ E3_ArrowFunctions_This/example1.js
@@ -1,16 +1,39 @@
+/*
+  - Demonstrating how this behaves inside an arow functions.
+*/
+
+// here we have an object called me.
+
 let me = {
     firstName: 'Tony',
+
+    // the following method inside an object called me
+    // has a function expression inside of it 
+    // and calling the function again 
+
     greet: function() {
+        // based on the way greet is called
+        // the following code refere this as the object calling greet - me.
         console.log(this);
-        
+
         let logMe = function() {
-            console.log(this);
+            console.log(this); // so the output will be the window object.
         }
+
+        // but here logMe is behaving differently.
+        // a regular function logMe has this in its execution context which refers window in non strict mode
+        // and refers 'undefine' in strict mode 
         logMe();
     },
     greet2: function() {
-        console.log(this);
-        
+        console.log(this); // this refers me.
+
+        // here incase of an arrow function 
+        // it has no this inside of its execution context 
+        // therefore using its reference power 
+        // it grabs this from the outer excution contexct lexically.
+        // so this in such case will be what is defined inside the outer greet ()
+        // the current object me.
         let logMe = () => {
             console.log(this);
         }
@@ -20,3 +43,6 @@ let me = {
 
 me.greet();
 me.greet2();
+
+// in general use arrow function expression 
+// except the use of standard function expression is required.
